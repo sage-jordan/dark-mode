2c64c51302bc7b7f183560a7d92ff6ee1e126dfa
sage-jordan <sagemjrodan@gmail.com>
2019-09-11 20:49:56 -0400

    Created/Finished useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import JSDOMEnvironment from 'jest-environment-jsdom';
+
+export const useLocalStorage = (key, initValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSDOMEnvironment.parse(item) : initValue;
+    });
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+
+    return [storedValue];
+};;
